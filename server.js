07cfca7effc662b0b05bbaa5fb1a6cc82e3262f2
Bipin <131430622+beepin007@users.noreply.github.com>
2024-10-27 14:13:31 +0530

    added cors

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const nodemailer = require("nodemailer");
+const cors = require("cors");
 
 const PORT = 5000;
 const app = express();
 
 app.use(express.json());
+app.use(cors("*"));
 
 const sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
