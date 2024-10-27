b47b0927f193f23307f3d209c73112001f05dbf0
Bipin <131430622+beepin007@users.noreply.github.com>
2024-10-27 13:13:15 +0530

    created api for contact

---
+++ server.js
@@ -0,0 +1,44 @@
+require("dotenv").config();
+const express = require("express");
+const nodemailer = require("nodemailer");
+
+const PORT = 5000;
+const app = express();
+
+app.use(express.json());
+
+const sendEmail = async (options) => {
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+  };
+
+  await transporter.sendMail(mailOptions);
+};
+
+app.post("/send-email", async (req, res) => {
+  try {
+    const { email, subject, message, name } = req.body;
+    await sendEmail({ email, subject, message, name });
+    res.status(200).send({ message: "Email sent successfully" });
+  } catch (error) {
+    console.error("Error sending email:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
